96070489ec873414e8738319e1cd842de6af74f5
gqueste <gabriel.queste@gmail.com>
2014-06-29 16:38:11 +0200

    Routing mis en place

--- app/app.js
+++ app/app.js
@@ -1,6 +1,23 @@
-function StatutsCtrl($scope, $http) {
+angular.module('Storytime', ['ngRoute'])
+
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller:'StatutsCtrl',
+      templateUrl:'./statuts.html'
+    })
+    .when('/ii', {
+      controller:'StatutsCtrl',
+      templateUrl:'./ii.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
+
+.controller('StatutsCtrl', function($scope, $http) {
 	 $http.get("./ajax/getStatuts.php").success(function(data){
 			$scope.statuts = data;
 		});
-};
+});
 
